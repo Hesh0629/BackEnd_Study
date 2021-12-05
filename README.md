a797dda57a5642abde62bbbdbd0e00a6d4ea1651
한성환 <hesh0629@kakao.com>
2021-12-05 12:55:24 +0000

    edit storage

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Node.js_Study  
 <ul>
 	<li>node.js docs: https://nodejs.org/api/worker_threads.html</li>
+	<li>https://github.com/ZeroCho/nodejs-book</li>
 </ul>
---
+++ chapter4/createServer.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const server = http.createServer(async (req, res) => {
+	// res.writeHead(statusCode[, statusMessage][, headers])
+	try{
+		const data = await fs.readFile('./createServer.html');
+		res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+		res.end(data);
+	} catch(err){
+		console.error(err);
+		res.writeHead(500, { 'Content-Type': 'text/html; charset=utf-8' });
+		res.end(err.message);
+	}
+});
+
+server.listen(8080);
+
+server.on('listening', () => {
+	console.log('waiting for server at port 8080');
+});
+server.on('error', (error) => {
+	console.log(error);
+});
