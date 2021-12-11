b25cac80c5e9aae050eadedc08e1dd9ae775b347
한성환 <hesh0629@kakao.com>
2021-12-11 15:31:58 +0000

    finish SQL chapter

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -5,8 +5,12 @@ const nunjucks = require('nunjucks');
 
 const { sequelize } = require('./models');
 
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
+
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
@@ -24,6 +28,10 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/',indexRouter);
+app.use('/users',usersRouter);
+app.use('/comments',commentsRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
--- learn-sequelize/models/comment.js
+++ learn-sequelize/models/comment.js
@@ -25,10 +25,9 @@ module.exports = class Comment extends Sequelize.Model {
 			}
 		);
 	}
-
 	static associate(db) {
-		// 1:N 관계에서 Comment row를 가져올 때 연관된 단 하나의 User row를 가져옴
+			// 1:N 관계에서 Comment row를 가져올 때 연관된 단 하나의 User row를 가져옴
 		// foreignKey로 Comment의 commenter가 User의 id로 설정되어 있으므로 id는 targetKey가 된다.
-		db.User.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
-	}
+    db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
+  }
 };
